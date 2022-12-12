42ae70c807e07b2afac19a27b00306d21460aed6
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-12-12 10:45:05 -0500

    modified code to display weather information

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ function weatherInformation(cityName) {
                     <img src="http://openweathermap.org/img/w/${res.list[0].weather[0].icon}.png">
                     <div class="displayWeatherInformationCards">
                         <h5 class="cardTitle">${cityName}</h5>
-                        <p class="cardText">Tempreture: ${res.main.temp}<span>${res.weather[0].description}</span></p>
-                        <p class="cardText">Humidity: ${res.main.humidity}</p>
-                        <p class="cardText">Wind Speed: ${res.wind.speed}</p>
+                        <p class="cardText">Tempreture: ${res.list[0].main.temp}<span>${res.list[0].weather[0].description}</span></p>
+                        <p class="cardText">Humidity: ${res.list[0].main.humidity}</p>
+                        <p class="cardText">Wind Speed: ${res.list[0].wind.speed}</p>
                     </div>
                 </div>
             `
